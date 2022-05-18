98b2a6cca1961168dcda37002357d02c74da9922
MKipruto12 <mercykipruto2017@gmail.com>
2022-05-18 13:01:59 -0700

    modified

--- shell.c
+++ shell.c
@@ -1,155 +1,90 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * shell - UNIX shell
- *
- * @build: input build
+ * main - Simple Shell (Hsh)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: Exit Value By Status
  */
 
-void shell(config *build)
+int main(__attribute__((unused)) int argc, char **argv)
 {
-	while (true)
-	{
-		getInputAndPrompt(build);
-		if (splitString(build) == false)
-			continue;
-		if (findBuiltIns(build) == true)
-			continue;
-		checkPath(build);
-		forkAndExecute(build);
-	}
-}
+	char *input, **cmd;
+	int counter = 0, statue = 1, st = 0;
 
-/**
- * getInputAndPrompt - check stdin and retrieves next line; handles
- * prompt display
- * @build: input build
- */
-void getInputAndPrompt(config *build)
-{
-	register int len;
-	size_t bufferSize = 0;
-	char *ptr, *ptr2;
-
-	build->args = NULL;
-	build->envList = NULL;
-	build->lineCounter++;
-	if (isatty(STDIN_FILENO))
-		prompt();
-	len = getline(&build->buffer, &bufferSize, stdin);
-	if (len == EOF)
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
+	signal(SIGINT, signal_to_handel);
+	while (statue)
 	{
-		freeMembers(build);
+		counter++;
 		if (isatty(STDIN_FILENO))
-			newLine();
-		if (build->errorStatus)
-			exit(build->errorStatus);
-		exit(EXIT_SUCCESS);
+			prompt();
+		input = _getline();
+		if (input[0] == '\0')
+		{
+			continue;
+		}
+		history(input);
+		cmd = parse_cmd(input);
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			exit_bul(cmd, input, argv, counter);
+		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd, st);
+			free_all(cmd, input);
+			continue;
+		}
+		else
+		{
+			st = check_cmd(cmd, input, counter, argv);
 
+		}
+		free_all(cmd, input);
 	}
-	ptr = _strchr(build->buffer, '\n');
-	ptr2 = _strchr(build->buffer, '\t');
-	if (ptr || ptr2)
-		nullByte(build->buffer, len - 1);
-	stripComments(build->buffer);
+	return (statue);
 }
-
 /**
- * stripComments - remove comments from input string
+ * check_builtin - check builtin
  *
- * @str: input string
- * Return: length of remaining string
+ * @cmd:command to check
+ * Return: 0 Succes -1 Fail
  */
-void stripComments(char *str)
+int check_builtin(char **cmd)
 {
-	register int i = 0;
-	_Bool notFirst = false;
-
-	while (str[i])
+	bul_t fun[] = {
+		{"cd", NULL},
+		{"help", NULL},
+		{"echo", NULL},
+		{"history", NULL},
+		{NULL, NULL}
+	};
+	int i = 0;
+		if (*cmd == NULL)
 	{
-		if (i == 0 && str[i] == '#')
-		{
-			nullByte(str, i);
-			return;
-		}
-		if (notFirst)
-		{
-			if (str[i] == '#' && str[i - 1] == ' ')
-			{
-				nullByte(str, i);
-				return;
-			}
-		}
-		i++;
-		notFirst = true;
+		return (-1);
 	}
-}
 
-/**
- * forkAndExecute - fork current build and execute processes
- *
- * @build: input build
- */
-
-void forkAndExecute(config *build)
-{
-	int status;
-	pid_t f1 = fork();
-
-	convertLLtoArr(build);
-	if (f1 == -1)
+	while ((fun + i)->command)
 	{
-		perror("error\n");
-		freeMembers(build);
-		freeArgs(build->envList);
-		exit(1);
-	}
-	if (f1 == 0)
-	{
-		if (execve(build->fullPath, build->args, build->envList) == -1)
-		{
-			errorHandler(build);
-			freeMembers(build);
-			freeArgs(build->envList);
-			if (errno == ENOENT)
-				exit(127);
-			if (errno == EACCES)
-				exit(126);
-		}
-	} else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-			build->errorStatus = WEXITSTATUS(status);
-		freeArgsAndBuffer(build);
-		freeArgs(build->envList);
-	}
-}
-
-/**
- * convertLLtoArr - convert linked list to array
- * @build: input build
- */
-void convertLLtoArr(config *build)
-{
-	register int i = 0;
-	size_t count = 0;
-	char **envList = NULL;
-	l_list *tmp = build->env;
-
-	count = list_len(build->env);
-	envList = malloc(sizeof(char *) * (count + 1));
-	if (!envList)
-	{
-		perror("Malloc failed\n");
-		exit(1);
-	}
-	while (tmp)
-	{
-		envList[i] = _strdup(tmp->string);
-		tmp = tmp->next;
+		if (_strcmp(cmd[0], (fun + i)->command) == 0)
+			return (0);
 		i++;
 	}
-	envList[i] = NULL;
-	build->envList = envList;
+	return (-1);
+}
+/**
+ * creat_envi - Creat Array of Enviroment Variable
+ * @envi: Array of Enviroment Variable
+ * Return: Void
+ */
+void creat_envi(char **envi)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+		envi[i] = _strdup(environ[i]);
+	envi[i] = NULL;
 }

994f9bc30da4a7c664e1f4d2e307895b77723507
MKipruto12 <mercykipruto2017@gmail.com>
2022-05-18 14:33:19 -0700

    shell project

---
+++ shell.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * main -  prints "$ " and waits for the user to enter a command
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char cmd[20];
+
+	printf("$ ");
+	while (scanf("%s", cmd))
+	{
+		printf("%s\n", cmd);
+		system(cmd);
+		printf("$ ");
+	}
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#endif
